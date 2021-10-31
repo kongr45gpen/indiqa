800346dd6f8fb1c56e60187d00017b699e169488
kongr45gpen <electrovesta@gmail.com>
2021-10-31 22:38:54 +0200

    Add presenter view action buttons

--- imports/ui/PresenterQuestion.jsx
+++ imports/ui/PresenterQuestion.jsx
@@ -4,26 +4,35 @@ import { useState } from 'react';
 export const PresenterQuestion = ({ question }) => {
     return (
         <div class="frame my-4 w-100 presenter__question">
-            <div className={`frame__body p-0 ${question.status == "spotlight" ? "bg-yellow-200" : ""}`}>
-                <div class="u-pull-right m-3 question__vote text-indigo-700">
-                    <span class="icon subtitle mr-1">
-                        <i className={`fa-wrapper fa-heart far`}></i>
-                    </span>
-                    { question.votes }
+            <div className={`frame__body p-0 ${question.status == "spotlight" ? "bg-yellow-200 presenter__spotlight" : ""}`}>
+                <div className="u-pull-right m-5">
+                    <div className="text-purple-700">
+                        <span class="icon subtitle mr-1">
+                            <i className={`fa-wrapper fa-heart far`}></i>
+                        </span>
+                        { question.votes }
+                    </div>
+                    <div class="u-text-right presenter__actions">
+                        { question.status == "approved" &&
+                        <div>
+                            <i className={`fa-wrapper fa-lightbulb fa-fw far`}></i>
+                        </div>
+}
+
+                        <div>
+                            <i className={`fa-wrapper fa-check fa-fw fas`}></i>
+                        </div>
+
+                        { question.status == "spotlight" &&
+                        <div>
+                            <i className={`fa-wrapper fa-caret-down fa-fw fas`}></i>
+                        </div>
+}
+                    </div>
                 </div>
                 <div class="p-5 question__text">
                     { question.text }
                 </div>
-                { question.status == "answered" &&
-                    <div class="frame__footer bg-green-100">
-                        <div class="frame__subtitle">
-                            <span class="tag tag--success mr-1">
-                                <i class="fa-wrapper fa-check fas mr-1"></i>
-                                Answered</span>
-                            <span class="text-green-800">{ question.response }</span>
-                        </div>
-                    </div>
-                }
             </div>
         </div>
     )
