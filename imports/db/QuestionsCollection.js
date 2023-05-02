46cc3f25a32e7ba6a432b5cd2e1e431ddb595d92
kongr45gpen <electrovesta@gmail.com>
2023-05-02 15:44:42 +0200

    Better stream handling of long questions

--- imports/db/QuestionsCollection.js
+++ imports/db/QuestionsCollection.js
@@ -3,8 +3,6 @@ import { Mongo } from 'meteor/mongo';
 export const QuestionsCollection = new Mongo.Collection('questions');
 
 QuestionsCollection.getActiveQuestions = function() {
-    console.log("HELLO. I HAVE BEEN CALLED!")
-
     const spotlight = QuestionsCollection.findOne({ status: 'spotlight' });
     const approved = QuestionsCollection.find({ status: { $in: [ 'approved' ]} }, {
         sort: [['votes', 'desc'], ['createdAt', 'asc']]
--- imports/ui/Streaming.jsx
+++ imports/ui/Streaming.jsx
@@ -14,32 +14,28 @@ export const Streaming = () => {
   const { spotlighted } = useTracker(() => {
     Meteor.subscribe('questions.public');
 
-    return { spotlighted: QuestionsCollection.findOne({ status: 'spotlight'}) };
+    return { spotlighted: QuestionsCollection.findOne({ status: 'spotlight' }) };
   });
 
 
   return <div className="u-absolute u-top-0 w-screen h-screen">
-      { 
-        <div className="streaming__container">
-          <CSSTransition in={!!spotlighted} timeout={1400} classNames="streaming__transition">
-            <aside className="streaming__logo"></aside>
-          </CSSTransition>
-        <SwitchTransition>
-        {/* <TransitionGroup className="streaming__question_container"> */}
-        <CSSTransition in={!!spotlighted} timeout={700} classNames="streaming__transition"
-          key={ spotlighted ? spotlighted._id : 0 }>
-            { spotlighted ?
-          
-            <div className="streaming__question" id={ spotlighted._id }>
-              { spotlighted.text }
-          </div> : <div></div>
-          }
+    {
+      <div className="streaming__container">
+        <CSSTransition in={!!spotlighted} timeout={1400} classNames="streaming__transition">
+          <aside className="streaming__logo"></aside>
         </CSSTransition>
-        {/* </TransitionGroup> */}
+        <SwitchTransition>
+          <CSSTransition in={!!spotlighted} timeout={700} classNames="streaming__transition"
+            key={spotlighted ? spotlighted._id : 0}>
+            {spotlighted ?
+              <div className={`streaming__question ` + ((spotlighted.text.length > 80) ? "streaming__question_wordy" : "") } id={spotlighted._id}>
+                <span>{spotlighted.text}</span>
+              </div> : <div></div>
+            }
+          </CSSTransition>
         </SwitchTransition>
-        </div>
-        // </SwitchTransition>
-      }
-      </div>;
+      </div>
+    }
+  </div>;
 
 };
